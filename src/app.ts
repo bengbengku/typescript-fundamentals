2cb7de69d5dd4ab2396cee326e23a267b2de67df
Anggiat Benget Napitupulu <anggiatbenget70@gmail.com>
2023-01-23 21:20:34 +0700

    Added: Property Decorator

--- src/app.ts
+++ src/app.ts
@@ -32,3 +32,33 @@ class PersonDecorator {
 
 const pers = new PersonDecorator();
 console.log(pers);
+
+// --- Decorator property
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property Decorator!');
+  console.log(target, 'Property Name: ' + propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
